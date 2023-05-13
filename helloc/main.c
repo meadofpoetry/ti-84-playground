37ff85c1714c777a398899f442f38a775d2c7846
meadofpoetry <eugene.bulavin.se@gmail.com>
2023-05-14 00:12:40 +0400

    tinkering with time api

--- helloc/main.c
+++ helloc/main.c
@@ -1,12 +1,42 @@
 #include "../include/ti83.h"
 
+void
+fmt_time(int h, int m, int s, char *dst) {
+  dst[0] = ((h / 10) % 10) + '0';
+  dst[1] = (h % 10) + '0';
+  dst[2] = ':';
+  dst[3] = ((m / 10) % 10) + '0';
+  dst[4] = (m % 10) + '0';
+  dst[5] = ':';
+  dst[6] = ((s / 10) % 10) + '0';
+  dst[7] = (s % 10) + '0';
+  dst[8] = '\0';
+}
+
 int
 main() {
+  int h, m, s;
+  char date[10];
+
   clear_lcd_full();
-  cur_row = 0;
-  cur_col = 0;
-  puts("Hello, world");
-  new_line();
-  get_key();
+
+  while (!is_enter_pressed()) { 
+    cur_row = 0;
+    cur_col = 0;
+
+    get_time(&h, &m, &s);
+
+    fmt_time(h, m, s, date);
+    puts(date);
+    new_line();
+
+    get_time_string(date);
+    puts(date);
+    new_line();
+
+    puts("To exit press enter...");
+    new_line();
+  }
+  
   return 0;
 }
--- include/ti83.h
+++ include/ti83.h
@@ -9,6 +9,30 @@ clear_lcd_full() {
   __endasm;
 }
 
+inline void
+clear_lcd() {
+  __asm
+    rst 0x28
+    .dw 0x4543
+  __endasm;
+}
+
+inline void
+clear_scrn_full() {
+  __asm
+    rst 0x28
+    .dw 0x4546
+  __endasm;
+}
+
+inline void
+clear_scrn() {
+  __asm
+    rst 0x28
+    .dw 0x4549
+  __endasm;
+}
+
 void
 puts(char *s) {
   __asm
@@ -44,5 +68,98 @@ get_key() {
     .dw 0x4972
     ld h, #0x0
     ld l, a
+  __endasm;
+}
+
+void
+get_time_string(char *dst) {
+  __asm
+    ld e, 4(ix)
+    ld d, 5(ix)
+    ld a, 0x3F(iy)
+    push af
+    set 2, 0x3F(iy) // Set 24-hour format
+    set 4, 0x3F(iy) // Use ASCII
+    rst 0x28
+    .dw 0x515E
+    pop af
+    ld 0x3F(iy), a
+  __endasm;
+  (void)dst;
+}
+
+void
+get_time(int *h, int *m, int *s) {
+  __asm
+    // _getTime
+    rst 0x28
+    .dw 0x515B
+    // Seconds
+    // PopRealO1
+    rst 0x28
+    .dw 0x4390
+    // ConvOP1
+    rst 0x28
+    .dw 0x4AEF
+
+    ld l, 8(ix)
+    ld h, 9(ix)
+    ld (hl), e
+    inc hl
+    ld (hl), d
+
+    // Minutes
+    // PopRealO1
+    rst 0x28
+    .dw 0x4390
+    // ConvOP1
+    rst 0x28
+    .dw 0x4AEF
+
+    ld l, 6(ix)
+    ld h, 7(ix)
+    ld (hl), e
+    inc hl
+    ld (hl), d
+
+    // Hours
+    // PopRealO1
+    rst 0x28
+    .dw 0x4390
+    // ConvOP1
+    rst 0x28
+    .dw 0x4AEF
+
+    ld l, 4(ix)
+    ld h, 5(ix)
+    ld (hl), e
+    inc hl
+    ld (hl), d
+    
+  __endasm;
+}
+
+int
+is_enter_pressed() {
+  __asm
+    ld a, #0xFD
+    out (1), a
+    nop
+    nop
+    in a, (1)
+    bit 0, a
+    jr z, is_set
+    ld hl, #0
+    ret
+  is_set:
+    ld hl, #1
+  __endasm;
+}
+
+void
+lcd_busy() {
+  __asm
+    rst 0x28
+    .dw 0x4051
   __endasm;
 }
